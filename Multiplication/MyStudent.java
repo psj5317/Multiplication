f67973db284e1ed83e0349f053f18fb907c31ece
jjyoung0522 <jjyoung05220522@gmail.com>
2026-05-06 00:30:14 +0900

    변경받기

--- Multiplication/MyStudent.java
+++ Multiplication/MyStudent.java
@@ -2,12 +2,12 @@ package Multiplication;
 import java.util.Scanner;
 
 /**
- * 구구단을 답할 학생과 단수를 정해주면 해당 학생이 구구단을 답변하는 프로그램
+ * MyStudent 클래스의 설명을 작성하세요.
  *
- * @author (2023320010박성준, 2023320012강성하, 2023320006정준영, 2023320029정지후)
- * @version (2026.05.05)
+ * @author (작성자 이름)
+ * @version (버전 번호 또는 작성한 날짜)
  */
-public class MyApp
+public class MyStudent
 {   
     
     public static void main(String[] args) {
@@ -15,21 +15,21 @@ public class MyApp
         
         // 학생 개체 생성
         Student Jenna = new Student("Jenna","R005");
-        Student James = new Student("James","R010");
-        Student John = new Student("John","R009");
+        Student James = new Student("James","R009");
+        Student John = new Student("John","R010");
         Student Maria = new Student("Maria","R029");
         
         System.out.println("학생 이름을 입력해 주세요");
         String name = scan.next();
         
         if(name.equals("Jenna")) {
-            System.out.println("Jenna : " + Jenna.answerCalculate());
+            System.out.println(Jenna.answerCalculate());
         } else if (name.equals("James")) {
-            System.out.println("James : " + James.answerCalculate());
+            System.out.println(James.answerCalculate());
         } else if (name.equals("John")) {
-            System.out.println("John : " + John.answerCalculate());
+            System.out.println(John.answerCalculate());
         } else if (name.equals("Maria")) {
-            System.out.println("Maria : " + Maria.answerCalculate());
+            System.out.println(Maria.answerCalculate());
         } else {
             System.out.println("학생 이름을 확인해 주세요.");
         }
